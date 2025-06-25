cd3e843038122593b9015120c16d2c8fe55796ea
احمد ناجى رمضان درويش عمران <102424114+AhmedNagy24@users.noreply.github.com>
2025-06-25 04:58:50 +0300

    Update CV

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -72,7 +72,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1n_bsm_vv4r2BB3VOUGerJphsDCKy710i/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1BZiwM4EwXEeX2ruys907XHuA9vWPkTOK/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'C/C++',
